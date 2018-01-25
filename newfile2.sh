3b4c1c9a68f012a2194e307d4a8f988c6b84a4f1
itc134demo <nicolebadeofficehours@gmail.com>
2018-01-25 12:37:02 -0800

    fixed the direction of the slashes so they actually escape the quotes.  yay

--- newfile2.sh
+++ newfile2.sh
@@ -5,12 +5,12 @@ echo -e "Here are the steps I took to update this file \n
 2) cd hello-nti-300 \n
 3) Created the file newfile.sh \n
 4) git add newfile.sh \n
-5) git commit -m /"added a new example file/" \n
+5) git commit -m \"added a new example file\" \n
 6) git push \n
 7) Went to the website, refreshed, and the changes came through.  Then created another file via the website called newfile2.sh.  I then went back to my command line copy of the git repository, which was woefully out of date. \n
 8) git fetch \n
 9) git pull \n
 10) I edited newfile2.sh
 11) git add newfile2.sh
-12) git commit -m /"added useful content to newfile2.sh/" \n
+12) git commit -m \"added useful content to newfile2.sh\" \n
 13) git push"
