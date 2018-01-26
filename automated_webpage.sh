18c9abe90770e295522ec2e11e185089fd17e806
nic-instruction <root@apache-server.c.instructor-nti-300-2018.internal>
2018-01-26 04:12:42 +0000

    new updating web pages

---
+++ automated_webpage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "<html> \n<h1> Welcome, NTI-300 </h1> \n<h2> yay, I'm having so much fun with linux</h2> \n <p> this is the date right now: $(date +%c)</p> \n</html>" > /var/www/html/index.html
+
