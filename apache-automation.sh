3d03127cc5dff089f6daaad34366c89bc4fc8532
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-09-26 20:18:13 -0700

    Create new automation
    This will install apache and mod_ssl and comment out the standard welcome page and replace it with a custom welcome page.

---
+++ apache-automation.sh
@@ -0,0 +1,5 @@
+yum -y install httpd mod_ssl                                                            # Install apache and SSL support
+systemctl start httpd                                                                   # Start apache
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                         # Comment out the default welcome page
+echo "<html><body><h1>Hi there NTI 300</h1></body></html>" > /var/www/html/index.html   # Create custom welcome page
+systemctl restart httpd                                                                 # Restart apache so our changes take effect
