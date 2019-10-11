b3f04573b85f94b80f5dcccb20299277856a6985
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-10-10 18:08:40 -0700

    Update apache-automation.sh

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+if [ -e /etc/httpd ]; then #my comment here
+  exit 0                   # another comment 
+fi                         # closes the if statment
 yum -y install httpd mod_ssl                                                            # Install apache and SSL support
 systemctl start httpd                                                                   # Start apache
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                         # Comment out the default welcome page
