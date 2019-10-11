fd9478d6ebdc2c157262c2bd0dfbfc838fbd4cdc
nic-instruction <nicole.bade@seattlecolleges.edu>
2019-10-10 18:48:41 -0700

    Update packages2.sh

--- packages2.sh
+++ packages2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -e /usr/bin/wget ]; then 
+  exit 0;
+fi
 yum -y install wget
 wget https://raw.githubusercontent.com/nic-instruction/hello-nti-300/master/packages.txt
 for packages in $(cat packages.txt); do 
