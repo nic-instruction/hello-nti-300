0828affaff261ad19c4107f7ad680dcda5ae321c
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-11-18 08:16:57 -0800

    Delete automate_apache.sh
    double

--- automate_apache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum -y install httpd mod_ssl
-systemctl enable httpd
-systemctl start httpd
-
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
-
-echo -e "<html> \n<h1> Welcome, NTI-300 </h1> \n<h2> yay, I'm having so much fun with linux</h2> \n</html>" > /var/www/html/index.html
